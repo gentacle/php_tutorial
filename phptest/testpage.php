d9a46b6596c51d0aea7e30ad0eb770dda765aebd
gentacle <gentacle00@gmail.com>
2017-05-03 17:22:49 +0900

    retap

--- phptest/testpage.php
+++ phptest/testpage.php
@@ -6,12 +6,12 @@
   </head>
   <body>
     <?php
-// 패턴 구분자 뒤의 "i"는 대소문자를 구별하지 않게 합니다.
-if (preg_match("/php/i", "PHP is the web scripting language of choice.")) {
-    echo "발견하였습니다.";
-} else {
-    echo "발견하지 못했습니다.";
-}
+    if (preg_match("/php/i", "PHP is the web scripting language of choice.")) {
+      echo "발견하였습니다.";
+      } else {
+      echo "발견하지 못했습니다.";
+    }
+    // 패턴 구분자 뒤의 "i"는 대소문자를 구별하지 않게 합니다.
 ?>
   </body>
 </html>
