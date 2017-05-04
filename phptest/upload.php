ca452ebccee38ae4feefd8bf5bedcb4ba59ccdf6
gentacle <gentacle00@gmail.com>
2017-05-04 09:46:31 +0900

    complete upload.php. let's cookie.

---
+++ phptest/upload.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk=1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+
+    //file upload.
+    if (isset($_POST["submit"])) {
+      $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+      if($check !== false){
+        echo "File is an image - " . $check["mime"] . "." ;
+        $uploadOk = 1;
+      }else{
+        echo "File is not an image.";
+        $uploadOk = 0;
+      }
+    }
+
+    //check if file already exists.
+    if (file_exists($target_file)) {
+      echo "Sorry, file already exists.";
+      $uploadOk = 0;
+    }
+
+    //check file formats
+    if ($imageFileType != "jpg" &&
+        $imageFileType != "png" &&
+        $imageFileType != "jpeg" &&
+        $imageFileType != "gif") {
+          echo "Sorry. only JPG, JPEG, PNG & GIF files are allowed.";
+          $uploadOk = 0;
+    }
+
+    if ($uploadOk == 0) {
+      echo "Sorry. your file was not uploaded.";
+    }else {
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+      }else{
+        echo "Sorry. there was an error uploading your file.";
+      }
+    }
+
+    // //check file size.
+    // if($_FILE["fileToUpload"]["size"] > 500000){
+    //   echo "Sorry. fule size is too large.";
+    //   $uploadOk = 0;
+    // }
+    ?>
+  </body>
+</html>
